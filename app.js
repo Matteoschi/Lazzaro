75ec1499847babdcf969934f05f3f2ee9c4019ef
Matteo <94646702+Matteoschi@users.noreply.github.com>
2023-03-08 19:42:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("morti").style.display = "block";
+    document.getElementById("morti").scrollIntoView();
+}
+function myFunction1() {
+    document.getElementById("morti").style.display = "none";
+}
